e19d6f851adcaf32142f7f9fc553f521c92326bc
khanhlh1706 <lohungkhanh1495@gmail.com>
2023-02-23 01:50:03 +0700

    add get first n natural number function

--- demo-mstest-swp/MathUtils.cs
+++ demo-mstest-swp/MathUtils.cs
@@ -8,5 +8,17 @@ namespace demo_mstest_swp
 {
     public static class MathUtils
     {
+        //Find Sum of first N natural number
+        //Input: n
+        //Output: sum = 1 + 2 + ... + (n-1) + n 
+        public static int GetSumNFirstNum(int n)
+        {
+            int sum = 0;
+            for(int i = 1; i <= n; i++)
+            {
+                sum += i;
+            }
+            return sum;
+        }
     }
 }
--- demo-mstest-swp/Program.cs
+++ demo-mstest-swp/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace demo_mstest_swp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Test
+            int rs = MathUtils.GetSumNFirstNum(10);
+            Console.Write("Sum of 10 first natural number: " + rs);
+        }
+    }
+}
