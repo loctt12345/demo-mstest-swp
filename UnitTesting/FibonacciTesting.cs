1254936c130d78fabfc06ec8f8c9cb44ba94c15a
tanpnse2k2 <phannhattan201447@gmail.com>
2023-03-05 17:43:06 +0700

    fibonacci testing

---
+++ UnitTesting/FibonacciTesting.cs
@@ -0,0 +1,50 @@
+﻿using demo_mstest_swp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTesting
+{
+    [TestClass]
+    public class FibonacciTesting
+    {
+        [TestMethod]
+        public void testFibonacciGivenRightArgumentRunsWell()
+        {
+            // 1 1 2 3 5 8
+            FibonacciCalculator fibo = new FibonacciCalculator();
+            Assert.AreEqual(1, fibo.Fibonacci_Recursion(1));
+            Assert.AreEqual(5, fibo.Fibonacci_Recursion(5));
+            Assert.AreEqual(8, fibo.Fibonacci_Recursion(6));
+
+
+
+
+            /*Assert.Equals(5, fibo.Fibonacci_Recursion(5));
+            Assert.Equals(8, fibo.Fibonacci_Recursion(6));*/
+
+
+
+        }
+        [TestMethod]
+        public void testFibonacciUpgradeGivenRightArgumentRunsWell()
+        {
+            // 1 1 2 3 5 8
+            FibonacciCalculator fibo = new FibonacciCalculator();
+            Assert.AreEqual(1, fibo.Fibonacci_Upgrade(1));
+            Assert.AreEqual(5, fibo.Fibonacci_Upgrade(5));
+            Assert.AreEqual(8, fibo.Fibonacci_Upgrade(6));
+
+
+
+
+            /*Assert.Equals(5, fibo.Fibonacci_Recursion(5));
+            Assert.Equals(8, fibo.Fibonacci_Recursion(6));*/
+
+
+
+        }
+    }
+}
---
+++ demo-mstest-swp/FibonacciCalculator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo_mstest_swp
+{
+    public class FibonacciCalculator
+    {
+
+        public long Fibonacci_Recursion(int i)
+        {
+            if (i < 3) return 1;
+            return (Fibonacci_Recursion(i - 2) + Fibonacci_Recursion(i - 1));
+        }
+
+        public long Fibonacci_Upgrade(int x)
+        {
+               
+            // 1 1
+            // 1 2
+            // 2 3
+            if (x < 3) return 1;
+            long result = -9999999;
+            long before = 1;
+            long after = 1;
+            for (int i = 3;  i <= x; i ++)
+            {
+                result = before + after;
+                before = after;
+                after = result;
+
+            }
+
+            return result;
+
+
+        }
+    }
+}
